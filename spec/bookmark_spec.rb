716685be9a41c8ce3c9849616b334981ebd5af92
krisn64209 <krisnorris@gmail.com>
2019-11-06 15:06:25 +0000

    added test enviroment

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,10 +7,19 @@ describe Bookmark do
       expect(a_bookmark).to be_an_instance_of Bookmark
   end
   describe '.all' do
-    it 'returns all bookmarks' do
+    it 'returns a list of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+  
+      # Add the test data
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+  
       bookmarks = Bookmark.all
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.google.com")
+  
+      expect(bookmarks).to include('http://www.makersacademy.com')
+      expect(bookmarks).to include('http://www.destroyallsoftware.com')
+      expect(bookmarks).to include('http://www.google.com')
     end
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,11 +1,23 @@
+require 'pg'
+
 feature 'Creating a Bookmark' do
   scenario 'Visiting index page' do
     visit '/'
     expect(page).to have_content 'Bookmark Manager'
   end
   
-  scenario 'Checking bookmarks are on /bookmarks page' do
-    visit '/bookmarks'
-    expect(page).to have_content 'http://www.makersacademy.com'
+  scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
+    visit('/bookmarks')
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
