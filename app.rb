4509b80f5365734ba61cb25b307d2d51d5eac291
krisn64209 <krisnorris@gmail.com>
2019-11-07 16:41:10 +0000

    added edit function

--- app.rb
+++ app.rb
@@ -20,9 +20,18 @@ class BookmarkManager < Sinatra::Base
   end
 
   delete '/bookmarks/:id' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("DELETE FROM bookmarks WHERE id = '#{params['id']}'")
+    Bookmark.delete(id: params[:id])
     redirect '/bookmarks'
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edit"
   end
 
   run! if app_file == $0
